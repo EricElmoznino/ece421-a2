b351664a547084feedb2e678031e50ea5eb20828
Eric Elmoznino <ericelmoznino@modiface.com>
2019-02-21 15:22:57 -0500

    Only remaining thing in starter.py is to compute cross entropy gradiant

--- starter.py
+++ starter.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import os
 
@@ -16,9 +15,9 @@ def load_data():
         np.random.shuffle(rand_indx)
         data = data[rand_indx] / 255.0
         target = target[rand_indx]
-        train_data, train_target = data[:10000], target[:10000]
-        valid_data, valid_target = data[10000:16000], target[10000:16000]
-        test_data, test_target = data[16000:], target[16000:]
+        train_data, train_target = data[:10000].reshape((-1, 28 * 28)), target[:10000]
+        valid_data, valid_target = data[10000:16000].reshape((-1, 28 * 28)), target[10000:16000]
+        test_data, test_target = data[16000:].reshape((-1, 28 * 28)), target[16000:]
     return train_data, valid_data, test_data, train_target, valid_target, test_target
 
 
@@ -47,19 +46,21 @@ def shuffle(train_data, train_target):
 
 
 def relu(x):
-    pass
+    return x.clip(min=0)
 
 
 def softmax(x):
-    pass
+    x = np.exp(x)
+    return x / np.linalg.norm(x, axis=1, keepdims=True)
 
 
-def compute_layer(X, W, b):
-    pass
+def compute_layer(x, w, b):
+    return np.dot(x, w) + b
 
 
-def ce(target, prediction):
-    pass
+def average_ce(target, prediction):
+    ce = (target * np.log(prediction)).sum(axis=1)
+    return -ce.mean()
 
 
 def grad_ce(target, prediction):

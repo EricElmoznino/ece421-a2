da04b3a3a9b51482a683719fd72c627630f83e1e
Eric Elmoznino <ericelmoznino@modiface.com>
2019-02-21 14:16:34 -0500

    Reformatted file to conform to style standards

--- starter.py
+++ starter.py
@@ -3,61 +3,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+
 # Load the data
-def loadData():
+def load_data():
     with np.load("notMNIST.npz") as data:
-        Data, Target = data["images"], data["labels"]
+        data, target = data["images"], data["labels"]
         np.random.seed(521)
-        randIndx = np.arange(len(Data))
-        np.random.shuffle(randIndx)
-        Data = Data[randIndx] / 255.0
-        Target = Target[randIndx]
-        trainData, trainTarget = Data[:10000], Target[:10000]
-        validData, validTarget = Data[10000:16000], Target[10000:16000]
-        testData, testTarget = Data[16000:], Target[16000:]
-    return trainData, validData, testData, trainTarget, validTarget, testTarget
+        rand_indx = np.arange(len(data))
+        np.random.shuffle(rand_indx)
+        data = data[rand_indx] / 255.0
+        target = target[rand_indx]
+        train_data, train_target = data[:10000], target[:10000]
+        valid_data, valid_target = data[10000:16000], target[10000:16000]
+        test_data, test_target = data[16000:], target[16000:]
+    return train_data, valid_data, test_data, train_target, valid_target, test_target
+
 
 # Implementation of a neural network using only Numpy - trained using gradient descent with momentum
-def convertOneHot(trainTarget, validTarget, testTarget):
-    newtrain = np.zeros((trainTarget.shape[0], 10))
-    newvalid = np.zeros((validTarget.shape[0], 10))
-    newtest = np.zeros((testTarget.shape[0], 10))
+def convert_one_hot(train_target, valid_target, test_target):
+    new_train = np.zeros((train_target.shape[0], 10))
+    new_valid = np.zeros((valid_target.shape[0], 10))
+    new_test = np.zeros((test_target.shape[0], 10))
 
-    for item in range(0, trainTarget.shape[0]):
-        newtrain[item][trainTarget[item]] = 1
-    for item in range(0, validTarget.shape[0]):
-        newvalid[item][validTarget[item]] = 1
-    for item in range(0, testTarget.shape[0]):
-        newtest[item][testTarget[item]] = 1
-    return newtrain, newvalid, newtest
+    for item in range(0, train_target.shape[0]):
+        new_train[item][train_target[item]] = 1
+    for item in range(0, valid_target.shape[0]):
+        new_valid[item][valid_target[item]] = 1
+    for item in range(0, test_target.shape[0]):
+        new_test[item][test_target[item]] = 1
+    return new_train, new_valid, new_test
 
 
-def shuffle(trainData, trainTarget):
+def shuffle(train_data, train_target):
     np.random.seed(421)
-    randIndx = np.arange(len(trainData))
-    target = trainTarget
-    np.random.shuffle(randIndx)
-    data, target = trainData[randIndx], target[randIndx]
+    rand_indx = np.arange(len(train_data))
+    target = train_target
+    np.random.shuffle(rand_indx)
+    data, target = train_data[rand_indx], target[rand_indx]
     return data, target
 
 
 def relu(x):
-    # TODO
+    pass
+
 
 def softmax(x):
-    # TODO
+    pass
 
 
-def computeLayer(X, W, b):
-    # TODO
+def compute_layer(X, W, b):
+    pass
 
-def CE(target, prediction):
 
-    # TODO
+def ce(target, prediction):
+    pass
 
-def gradCE(target, prediction):
 
-    # TODO
-
+def grad_ce(target, prediction):
+    pass
